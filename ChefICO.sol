4e8041a7bf9d4aa656eff94299c62a09fe16a2a0
ivangrbavac <ivan.grbavac@energit.biz>
2018-05-02 02:51:16 +0200

    Update ChefICO.sol

--- ChefICO.sol
+++ ChefICO.sol
@@ -172,7 +172,7 @@ contract ChefICO {
     
    function safeWithdrawal() public afterICOdeadline {
         if (!softCapReached) {
-	          uint256 amount = balanceOf[msg.sender];
+	    uint256 amount = balanceOf[msg.sender];
             balanceOf[msg.sender] = 0;
             if (amount > 0) {
                 msg.sender.transfer(amount);
